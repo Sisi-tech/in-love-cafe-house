a84f0ef3a30f8eb1c4d180329f8ec4dd79c509fa
Sisi Wang <sisiwang242@gmail.com>
2024-02-20 23:21:13 -0500

    update banner image

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,9 +3,9 @@ import CafeBanner from './assets/CafeBanner.png';
 
 export default function Home() {
     return (
-        <div className='flex h-12 w-5/6 sm:w-4/5 justify-center '>
-            <div>
-                <img src={CafeBanner} alt="banner" className='w-full rounded-2xl' style={{ maxHeight: '300px'}}/>
+        <div className='flex justify-center '>
+            <div className='w-4/5'>
+                <img src={CafeBanner} alt="banner" className='w-full rounded-2xl' style={{ height: 'auto'}}/>
             </div>
         </div>
     );
