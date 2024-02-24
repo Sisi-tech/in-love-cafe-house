d9fbd1fadb7149f51ad45dd701a083b7b1ab2069
Sisi Wang <sisiwang242@gmail.com>
2024-02-24 06:40:21 -0500

    update contact page

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,3 +1,6 @@
+import { FaLinkedin, FaInstagram, FaFacebook } from 'react-icons/fa';
+import {HiOutlineMail} from 'react-icons/hi';
+
 
 export default function Contact() {
     return (
@@ -6,21 +9,53 @@ export default function Contact() {
                 <source src="src/assets/cafeHouse.mp4" type="video/mp4" />
             </video>
             <div className="border border-orange-950 w-4/5 m-10"></div>
-            <div className="grid sm:grid-cols-1 lg:grid-cols-4 justify-between lg:gap-14 sm:gap-6">
-                <div className="lg:w-[160px] lg:h-[160px] sm:w-[100px] sm:h-[100px] rounded-full overflow-hidden lg:mr-10">
-                    <video autoPlay loop muted className="w-full h-full object-cover">
-                        <source src="src/assets/Coffees.mp4" type="video/mp4"/>
-                    </video>
+            <div>
+                <div>
+                    <ul className='flex gap-16 mb-16'>
+                        <li className='w-[50px] h-[50px] rounded-lg flex justify-center items-center bg-[#2b2be4] text-white text-4xl shadow-md shadow-gray-400 hover:scale-110'>
+                            <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+                                <FaFacebook />
+                            </a>
+                        </li>
+                        <li className='w-[50px] h-[50px] rounded-lg flex justify-center items-center bg-[#E1306C] text-white text-4xl shadow-md shadow-gray-400 hover:scale-110'>
+                            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+                                <FaInstagram />
+                            </a>
+                        </li>
+                        <li className='w-[50px] h-[50px] rounded-lg flex justify-center items-center bg-[#0077B5] text-white text-4xl shadow-md shadow-gray-400 hover:scale-110'>
+                            <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
+                                <FaLinkedin />
+                            </a>
+                        </li>
+                        <li className='w-[50px] h-[50px] rounded-lg flex justify-center items-center bg-[#0074e4] text-white text-4xl shadow-md shadow-gray-400 hover:scale-110'>
+                            <a href="mailto:InLoveCafe@gmail.com" target="_blank" rel="noopener noreferrer">
+                                <HiOutlineMail />
+                            </a>
+                        </li>
+                    </ul>
                 </div>
+            </div>
+            <div className="grid sm:grid-cols-1 lg:grid-cols-3 justify-between lg:gap-14 sm:gap-6">
                 <div>
                     <h2 className="text-2xl pb-6">Location</h2>
-                    <p>123 North blvd</p>
-                    <p>New York, NY 12345</p>
+                    <a
+                        href="https://www.google.com/maps/place/136-17+39th+Ave+Flushing,+NY+11354"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="hover:underline"
+                    >
+                        <p>136-17 39th Ave</p> 
+                        <p>Flushing, NY 11354</p>
+                    </a>
                 </div>
                 <div>
                     <h2 className="text-2xl pb-6">Contact</h2>
-                    <p>InLoveCafe@gmail.com</p>
-                    <p>888-888-888</p>
+                    <div className="pb-2">
+                        <a href="mailto:InLoveCafe@gmail.com" className="hover:underline">InLoveCafe@gmail.com</a>
+                    </div>
+                    <div>
+                        <a href="tel:888-888-888" className="hover:underline">888-888-888</a>
+                    </div>
                 </div>
                 <div>
                     <h2 className="text-2xl pb-6">Hours</h2>
